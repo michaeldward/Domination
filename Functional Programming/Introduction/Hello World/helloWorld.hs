b329501438cbc627207f29f9083bf3bf4d1b8c9d
mikethebro <xenophon.of.athens@gmail.com>
2017-07-12 09:50:44 -0600

    hello world in Haskell

---
+++ Functional Programming/Introduction/Hello World/helloWorld.hs
@@ -0,0 +1,9 @@
+-- https://www.hackerrank.com/challenges/fp-hello-world
+
+-- Prints "Hello World"
+hello_world = putStrLn "Hello World"
+
+-- main function; executed by default
+main = do
+   hello_world
+
